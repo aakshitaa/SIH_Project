4f7a43c7f84da1ef4e2cdd4d07be42aca4374adf
aakshitaa <aakshitasingh786@gmail.com>
2023-12-18 17:24:58 +0530

    login page

--- sih-project/src/Components/Login/Login.js
+++ sih-project/src/Components/Login/Login.js
@@ -1,42 +1,128 @@
-import React from 'react';
-import './Login.css';
-import { Button } from '@mui/material';
-import { useMsal } from "@azure/msal-react";
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-export default function Login(props) {
+// TODO remove, this demo shouldn't need to reset the theme.
 
-  const { instance } = useMsal();
-  const navigate = useNavigate(); // Get the navigate function from useNavigate
+const defaultTheme = createTheme();
 
-  const handleLogin = async () => {
-    try {
-      await instance.loginPopup({
-        redirectUri: "http://localhost:3000/", // Update this with the correct redirect URI
-      });
-
-      // After successful login, navigate to the home path
-      navigate('/home');
-    } catch (error) {
-      console.log("Error during login:", error);
-    }
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
   };
 
   return (
-    <div className={`bdy ${props.theme === 'dark' ? 'dark-mode' : ''}`}>
-      <div className='row'>
-        <div className='banner-ad col col-sm-6' style={{ color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}` }}>
-          <h2 className='title'><div style={{ color: '#9C27B0', fontSize:"80px" }}>UNI</div>  - <span className='found'>Connect</span></h2>
-          <h1 className='title' style={{ color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}` }}>Welcome !</h1>
-          <div className='login-btn'>
-           <Button onClick={handleLogin} variant="contained" color="secondary" style={{ textTransform: "none", borderRadius: "10px", fontFamily: "'Poppins', sans-serif", fontSize: "1.1rem" }}>Login with Outlook</Button>
-          </div>
-        </div>
-        <div className='banner col col-sm-6'>
-          <img src={"http://media.giphy.com/media/3oEdvaba4h0I536VYQ/giphy.gif"} alt='logo' className='logo' />
-        </div>
-      </div>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${require('./bg.png')})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor:'#e9efff' ,
+            backgroundSize: 'contain',
+            backgroundPosition: 'left',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Vault Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Vault Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
